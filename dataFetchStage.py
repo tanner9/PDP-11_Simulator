5025f872407edd493e69b9008765788a6bf0cd5e
Tanner <tanner9@pdx.edu>
2017-06-12 08:59:10 -0700

    Eliminate redundant reads

--- dataFetchStage.py
+++ dataFetchStage.py
@@ -9,33 +9,38 @@ class dataFetchStage:
 		self.debug = debug
 		self.lastAddress = 0
 
-	def fetchSingleData(self, reg, mode, isByte):
+	def fetchSingleData(self, reg, mode, isByte, effectiveAddressOnly=False):
+		data = 0
 		if(mode == 0):
 			data = self.regFile.readReg(reg, mode)
 			self.lastAddress = reg
 		else:
 			if(mode == 1 or mode == 2 or mode == 4):
 				effectiveAddress = self.regFile.readReg(reg, mode, isByte)
-				if(isByte):
-					data = self.mem.memoryReadByte(effectiveAddress, 0)
-				else:
-					data = self.mem.memoryRead(effectiveAddress, 0)
+				if(not effectiveAddressOnly):
+					if(isByte):
+						data = self.mem.memoryReadByte(effectiveAddress, 0)
+					else:
+						data = self.mem.memoryRead(effectiveAddress, 0)
 			elif(mode == 3 or mode == 5):
 				address = self.regFile.readReg(reg, mode)
 				effectiveAddress = self.mem.memoryRead(address, 0)
-				data = self.mem.memoryRead(effectiveAddress, 0)
+				if(not effectiveAddressOnly):
+					data = self.mem.memoryRead(effectiveAddress, 0)
 			elif(mode == 6):
 				offset = self.getImmediate()
 				regAddress = self.regFile.readReg(reg, mode)
 				offset = twos_comp(offset, 16)
 				effectiveAddress = regAddress+offset
-				data = self.mem.memoryRead(effectiveAddress, 0)
+				if(not effectiveAddressOnly):
+					data = self.mem.memoryRead(effectiveAddress, 0)
 			else:
 				offset = self.getImmediate()
 				regAddress = self.regFile.readReg(reg, mode)
 				address = regAddress+offset
 				effectiveAddress = self.mem.memoryRead(address, 0)
-				data = self.mem.memoryRead(effectiveAddress, 0)
+				if(not effectiveAddressOnly):
+					data = self.mem.memoryRead(effectiveAddress, 0)
 
 			self.lastAddress = effectiveAddress
 			if(self.debug == True):
@@ -47,19 +52,27 @@ class dataFetchStage:
 		data = []
 		numOperands = instruction.getNumOperands()
 		data.append(numOperands)
+		op = instruction.getMnemonic()
 		if(numOperands == 2):
 			if(self.debug == True):
 				print("Fetching data for two operand instruction")
 			reg = instruction.getReg()
 			data0 = self.fetchSingleData(reg[0], reg[1], isByte)
-			data1 = self.fetchSingleData(reg[2], reg[3], isByte)
+			if(op == "MOV"):
+				data1 = self.fetchSingleData(reg[2], reg[3], isByte, True)
+			else:
+				data1 = self.fetchSingleData(reg[2], reg[3], isByte)
 			data.append(data1)
 			data.append(data0)
 		elif(numOperands == 1):
 			if(self.debug == True):
 				print("Fetching data for one operand instruction")
 			reg = instruction.getReg()
-			data.append(self.fetchSingleData(reg[0], reg[1], isByte))
+			if(op != "JSR"):
+				operand = self.fetchSingleData(reg[0], reg[1], isByte)
+			else:
+				operand = self.fetchSingleData(reg[0], reg[1], isByte, True)
+			data.append(operand)
 			data.append(0)
 		else:
 			if(self.debug == True):
